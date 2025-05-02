9235eaf3a262b9ed9e90a3320be019887c977bc1
NAYAMAAAAT <atefmdnayamatullah@gmail.com>
2025-05-02 16:05:52 +0600

    section title for different sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../components/banner/Banner';
+import Category from '../components/category/Category';
 
 const Home = () => {
   return (
     <div>
    <Banner></Banner>
+   <Category></Category>
     </div>
   );
 };
