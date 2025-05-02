a49d9aa92ae8a53d70b5ad3e2a342f40689643a0
NAYAMAAAAT <atefmdnayamatullah@gmail.com>
2025-05-02 16:43:31 +0600

    Create Parallax and Add Testimonials with react rating & feature section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Banner from '../components/banner/Banner';
 import Category from '../components/category/Category';
+import PopularMenu from '../components/PopularMenu';
+import Features from '../components/Features';
+import Testimonial from '../components/Testimonial';
 
 const Home = () => {
   return (
     <div>
    <Banner></Banner>
    <Category></Category>
+   <PopularMenu></PopularMenu>
+   <Features></Features>
+   <Testimonial></Testimonial>
     </div>
   );
 };
